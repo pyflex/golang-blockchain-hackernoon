94c5581f2a7357813b476674b8aced823ca96c1d
Alexander Törnvall <alextorn.business@gmail.com>
2021-03-08 18:01:26 +0100

    Initial commit: getting started with web api

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/pyflex/go-blockchain-hackernoon
+
+go 1.15
+
+require (
+	github.com/btcsuite/btcd v0.21.0-beta // indirect
+	github.com/ethereum/go-ethereum v1.10.1
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+	golang.org/x/sys v0.0.0-20210305230114-8fe3ee5dd75b // indirect
+)
---
+++ models/main.go
@@ -0,0 +1,48 @@
+package models
+
+// Block data struct
+type Block struct {
+	BlockNumber       int64         `json:"blockNumber"`
+	Timestamp         uint64        `json:"timestamp"`
+	Difficulty        uint64        `json:"difficulty"`
+	Hash              string        `json:"hash"`
+	TransactionsCount int           `json:"transactionsCount"`
+	Transactions      []Transaction `json:"transactions"`
+}
+
+// Transaction data struct
+type Transaction struct {
+	Hash     string `json:"hash"`
+	Value    string `json:"value"`
+	Gas      uint64 `json:"gas"`
+	GasPrice uint64 `json:"gasPrice"`
+	Nonce    uint64 `json:"nonce"`
+	To       string `json:"to"`
+	Pending  bool   `json:"pending"`
+}
+
+// TransferEthRequest data struct
+type TransferEthRequest struct {
+	PrivKey string `json:"privKey"`
+	To      string `json:"to"`
+	Amount  int64  `json:"amount"`
+}
+
+// HashResponse data struct
+type HashResponse struct {
+	Hash string `json:"hash"`
+}
+
+// BalanceResponse data struct
+type BalanceResponse struct {
+	Address string `json:"address"`
+	Balance string `json:"balance"`
+	Symbol  string `json:"symbol"`
+	Units   string `json:"units"`
+}
+
+// Error data struct
+type Error struct {
+	Code    uint64 `json:"code"`
+	Message string `json:"message"`
+}
